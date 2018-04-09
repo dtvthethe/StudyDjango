ce1c718228aec88dfedcd8d0ea0049af3f169aba
DTVThe <dtvthe@gmail.com>
2018-04-10 01:21:45 +0700

    Publish Template

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^detail/(?P<question_id>[0-9]+$)',views.detail, name='detail')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    context = {
+        'title': 'Trang chủ',
+        'questions':get_list_or_404(Question)
+    }
+    return render(request,'index.html', context=context)
+
+def detail(request, question_id):
+    context = {
+        'title':'Detail ß',
+        'question': get_object_or_404(Question, pk=question_id)
+    }
+    return render(request,'detail.html',context=context)
